58c11bb86d09500755360f11d3ad0ed27f082d56
Rohan Drummond <drummond.rohan@gmail.com>
2026-01-24 12:50:58 +1300

    Write FizzBuzz and FizzBuzzRange functions

--- CSharpDojo.Problems/FizzBuzzKata.cs
+++ CSharpDojo.Problems/FizzBuzzKata.cs
@@ -1,3 +1,6 @@
+using System.IO.Pipelines;
+using System.Text;
+
 namespace CSharpDojo.Problems;
 
 /// <summary>
@@ -13,19 +16,29 @@ public static class FizzBuzzKata
     /// </summary>
     public static string FizzBuzz(int number)
     {
-        if (number % 3 == 0 && number % 5 ==0)
-        {
-            return "FizzBuzz";
-        }
+        // if (number % 3 == 0 && number % 5 ==0)
+        // {
+        //     return "FizzBuzz";
+        // }
+        // if (number % 3 == 0)
+        // {
+        //     return "Fizz";
+        // }
+        // if (number % 5 == 0)
+        // {
+        //     return "Buzz";
+        // }
+        // return number.ToString();
+        StringBuilder sb = new StringBuilder("");
         if (number % 3 == 0)
         {
-            return "Fizz";
+            sb.Append("Fizz");
         }
         if (number % 5 == 0)
         {
-            return "Buzz";
+            sb.Append("Buzz");
         }
-        return number.ToString();
+        return sb.ToString() == "" ? number.ToString() : sb.ToString();
     }
 
     /// <summary>
